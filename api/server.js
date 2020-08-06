417d4d959dbfb17786423c53f56a564131ada121
Julian Leon <jleon1992@gmail.com>
2020-08-06 12:54:17 -0400

    added / route

--- api/server.js
+++ api/server.js
@@ -7,6 +7,9 @@ const server = express();
 
 server.use(helmet());
 
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'welcome'})
+})
 server.use('/api', apiRouter);
 
 module.exports = server;
